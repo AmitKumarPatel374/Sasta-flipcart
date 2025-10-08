bca85a95392f4d51baf450184e64e83b65911c92
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-10-08 19:58:56 +0530

    add product delete controller

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createProductController } = require("../controllers/product.controllers");
+const { createProductController, getAllProductController, updateProductController, deleteProductController } = require("../controllers/product.controllers");
 const uploads = require("../config/database/multer");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/create-product",uploads.array("images",5),createProductController);
+router.get("/get-products",getAllProductController);
+router.put("/update-product/:product_id",uploads.array("images",5),updateProductController);
+router.delete("/delete-product/:product_id",authMiddleware ,deleteProductController);
 
 module.exports = router;
