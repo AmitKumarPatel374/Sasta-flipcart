5f79225f348756e2e89017fb777eaaca02ce1798
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-10-08 18:03:42 +0530

    added updateProduct Controller

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -1,5 +1,6 @@
 const productModel = require("../model/product.model");
 const ProductModel = require("../model/product.model");
+const sendFilesToStorage = require("../services/storage.service");
 
 
 const getAllProductController = async (req, res) => {
@@ -69,6 +70,61 @@ const createProductController = async (req, res) => {
             message: "Internal server error",
             error: error,
         });
+    }
+}
+
+const updateProductController = async (req, res) => {
+    try {
+        let product_id = req.params.product_id;
+
+        let { title, description, amount, currency } = req.body;
+
+        if (!product_id) {
+            return res.status(404).json({
+                message: "id not found",
+            })
+        }
+
+        let uploadedImage;
+
+        if (req.files) {
+            uploadedImage = await Promise.all(
+                req.files.map(async (elem) => {
+                    return await sendFilesToStorage(elem.buffer, elem.originalname);
+                })
+            )
+        }
+
+        let updatedProduct = await productModel.findByIdAndUpdate(
+            {
+                _id: product_id,
+            },
+            {
+                title,
+                description,
+                price: {
+                    amount,
+                    currency
+                },
+                images: uploadedImage.map((elem) => elem.url);
+            },
+            {
+                new: true
+            }
+
+        )
+
+        return res.status(200).json({
+            message: "products updated!",
+            product:updatedProduct
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error!",
+        })
+
     }
 }
 
