83d41609f1aab916f15e08f1941e5a6dd0e481a1
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-11-15 18:39:58 +0530

    feat: enhance HomeProductSlider with navigation functionality for category images

--- frontend/src/layouts/HomeProductSlider.jsx
+++ frontend/src/layouts/HomeProductSlider.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import Slider from "react-slick";
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import Slider from "react-slick"
 
 const HomeProductSlider = () => {
+  const navigate = useNavigate()
   const sliderSettings = {
     dots: true,
     infinite: true,
@@ -12,44 +14,52 @@ const HomeProductSlider = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: true,
-  };
+  }
+
+  const category = "Fashion"
 
   return (
     <div className="w-full h-[80vh] overflow-hidden relative rounded-xl mt-5 p-5 bg-gray-500">
       <Slider {...sliderSettings}>
         <img
+          onClick={() => navigate(`/${"Fashion"}`)}
           src="/Fashion.png"
           alt="fashion"
           className="w-full h-[75vh] object-cover rounded-xl"
         />
         <img
+          onClick={() => navigate(`/${"Home & Furniture"}`)}
           src="/ferniture.jpeg"
           alt="furniture"
           className="w-full h-[75vh] object-cover rounded-xl"
         />
         <img
+          onClick={() => navigate(`/${"Electronics"}`)}
           src="/electronocs.png"
           alt="electronics"
           className="w-full h-[75vh] object-cover rounded-xl"
         />
         <img
+          onClick={() => navigate(`/${"Beauty & Food"}`)}
           src="/beauty.jpeg"
           alt="beauty"
           className="w-full h-[75vh]object-cover rounded-xl"
         />
         <img
+          onClick={() => navigate(`/${"Grocery"}`)}
           src="/grocery.jpeg"
           alt="grocery"
           className="w-full h-[75vh] object-cover rounded-xl"
         />
         <img
+          onClick={() => navigate(`/${"Mobiles & Tablets"}`)}
           src="/mobile.jpeg"
           alt="mobile"
           className="w-full h-[75vh] object-cover object-top  rounded-xl"
         />
       </Slider>
     </div>
-  );
-};
+  )
+}
 
-export default HomeProductSlider;
+export default HomeProductSlider
