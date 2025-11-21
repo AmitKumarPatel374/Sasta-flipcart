0d11f4a12e71b9dca132cd8c9905901110e5fb6f
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-11-21 13:23:26 +0530

    added add address controller

--- backend/src/controllers/address.controller.js
+++ backend/src/controllers/address.controller.js
@@ -1,16 +1,15 @@
-const axios = require("axios");
+const axios = require("axios")
+const addressModel = require("../model/address.model")
 
 const pincodeController = async (req, res) => {
   try {
-    const { pin } = req.params;
-    const response = await axios.get(
-      `https://api.postalpincode.in/pincode/${pin}`
-    );
+    const { pin } = req.params
+    const response = await axios.get(`https://api.postalpincode.in/pincode/${pin}`)
 
-    const data = response.data[0];
+    const data = response.data[0]
 
     if (data.Status !== "Success") {
-      return res.status(400).json({ message: "Invalid PIN code" });
+      return res.status(400).json({ message: "Invalid PIN code" })
     }
 
     return res.status(200).json({
@@ -18,15 +17,53 @@ const pincodeController = async (req, res) => {
       state: data.PostOffice[0].State,
       district: data.PostOffice[0].District,
       area: data.PostOffice[0].Name,
-    });
-
+    })
   } catch (error) {
-    console.log("error in pin validation->", error);
+    console.log("error in pin validation->", error)
     return res.status(500).json({
       message: "Internal server error",
-      error
-    });
+      error: error,
+    })
   }
-};
+}
 
-module.exports = { pincodeController };
+const addAddressController = async (req, res) => {
+  try {
+    const { ownerName, mobile, pincode, state, city, buildingName, landmark } = req.body
+
+    if (!ownerName || !mobile || !pincode || !state || !city || !buildingName) {
+      return req.status(404).json({
+        message: "all feilds are required",
+      })
+    }
+
+    let add = new addressModel.create({
+      ownerName,
+      mobile,
+      pincode,
+      state,
+      city,
+      buildingName,
+      landmark,
+      userId: req.user._id,
+    })
+
+    if (!add) {
+      return res.status(400).json({
+        message: "something went wrong!",
+      })
+    }
+
+    return res.status(201).json({
+      message: "address added successfully!",
+    })
+  } catch (error) {
+    console.log("error in adding address->", error)
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error,
+    })
+  }
+}
+
+module.exports = { pincodeController }
--- backend/src/routes/address.routes.js
+++ backend/src/routes/address.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { pincodeController } = require("../controllers/address.controller");
+const { pincodeController, addAddressController } = require("../controllers/address.controller");
 
 const router = express.Router();
 
 router.get("/pin/:pin",pincodeController);
+router.post("/add",addAddressController);
 
 module.exports=router;
